b8c00f938c42da98985ab193382d1cd8d3cb464f
Fish hacker <157135548+couift@users.noreply.github.com>
2024-03-07 17:19:17 +0330

    Update code-maker.py

--- code-maker.py
+++ code-maker.py
@@ -4,16 +4,7 @@ from subprocess import call
 from sys import platform
 import time
 import datetime
-from colorama import Fore
-def clear():
-    if platform not in ('win32', 'cygwin'):
-        command = 'clear'
-    else:
-        command = 'cls'
-    try:
-        call(command, shell=True)
-    except OSError as e:
-        print(type(e).__name__, e)
+
 
 red='\033[31m'
 green='\033[32m'
